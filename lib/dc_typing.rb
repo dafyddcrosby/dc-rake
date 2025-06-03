d9130eb39ffe84f03c3ebc129e410b195cda46fc
David Crosby <dave@dafyddcrosby.ca>
2025-06-03 07:50:49 -0600

    Get rid of copypasta

--- lib/dc_typing.rb
+++ lib/dc_typing.rb
@@ -1,12 +1,3 @@
 # frozen_string_literal: true
 
-if ENV["COVERAGE"]
-  require "simplecov"
-  SimpleCov.start do
-    add_filter "/test/"
-  end
-end
-
-require_relative "dc_minitest/version"
-require "minitest/autorun"
-require "minitest/pride"
+require_relative "dc_typing/version"
